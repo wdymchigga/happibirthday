570beb224d14f07e275fd12ce2a55e4d0f10d727
wdymchigga <abhirawat0218@gmail.com>
2026-02-17 23:27:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ function scrollToSection(id) {
     behavior: "smooth"
   });
 }
+// Show back to top button
+window.onscroll = function() {
+  let btn = document.getElementById("topBtn");
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+// Scroll to top
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
